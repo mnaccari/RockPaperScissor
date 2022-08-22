0c6f0a298ebfe35e1ea8ffe71037c243acda478d
Marco Naccari <marco.nccr@gmail.com>
2022-08-22 21:05:18 +0200

    js functions added

---
+++ script.js
@@ -0,0 +1,40 @@
+const choices = ["rock", "paper", "scissor"];
+
+function getComputerChoice() {
+    return choices[Math.floor(Math.random()*choices.length)];
+}
+
+function playRound(playerSelection, computerSelection) {
+    let player = playerSelection.toLowerCase();
+    if (player == computerSelection){
+        return "It's a draw!";
+    }
+    if (player = "rock"){
+        if (computerSelection == "paper"){
+            return "You Lose! Paper beats Rock";
+        }
+        if (computerSelection == "scissor"){
+            return "You Win! Rock beats Scissor";
+        }
+    }
+    if (player = "paper"){
+        if (computerSelection == "scissor"){
+            return "You Lose! Scissor beats Paper";
+        }
+        if (computerSelection == "rock"){
+            return "You Win! Paper beats Rock";
+        }
+    }
+    if (player = "scissor"){
+        if (computerSelection == "rock"){
+            return "You Lose! Rock beats Scissor";
+        }
+        if (computerSelection == "paper"){
+            return "You Win! Scissor beats Paper";
+        }
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
